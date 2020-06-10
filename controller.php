3a48e2b8440e24089ba569111601076705b9cf60
Matthieu291 <61006095+Matthieu291@users.noreply.github.com>
2020-06-10 15:45:25 +0200

    up

--- controller.php
+++ controller.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$dbn = 'pgsql:dbname=projet;host=127.0.0.1,port=5432';
+$dbn = 'pgsql:dbname=projet;host=localhost;port=5432';
 $us = 'postgres';
 $pw = 'Isen2018';
 try{
@@ -16,8 +16,11 @@ catch (PDOException $e) {
 
 if (isset($_GET['func'])) {
     if ($_GET['func'] == 'validSearch') {
-        if (isset($_POST['originAirport']) && isset($_POST['destinationAirport']) && isset($_POST['dateDepart']) && isset($_POST['nom1']) && isset($_POST['prenom1']) && isset($_POST['dateNaissance1']) && isset($_POST['mail1'])) {
-            recupForm();
+        if (isset($_POST['originAirport']) && isset($_POST['destinationAirport']) && isset($_POST['dateDepart'])){// && isset($_POST['nom1']) && isset($_POST['prenom1']) && isset($_POST['dateNaissance1']) && isset($_POST['mail1'])) {
+            //mettre de l'ajax ici
+        }
+        else {
+            echo ("Il y a une erreur dans la saisie du formulaire");
         }
     }
     if ($_GET['func'] == 'confirm') {
@@ -25,16 +28,3 @@ if (isset($_GET['func'])) {
     }
 }
 
-
-function recupForm(){
-
-    $_SESSION['originAirport'] = $_POST['originAirport'];
-    $_SESSION['destinationAirport'] = $_POST['destinationAirport'];
-    $_SESSION['dateDepart'] = $_POST['dateDepart'];
-    for ($i=1; $i<=$_SESSION['nbrePassagers'];$i++){
-        $_SESSION['nom'.strval($i)] =$_POST['nom'.strval($i)];
-        $_SESSION['prenom'.strval($i)] =$_POST['prenom'.strval($i)];
-        $_SESSION['dateNaissance'.strval($i)] =$_POST['dateNaissance'.strval($i)];
-        $_SESSION['mail'.strval($i)] =$_POST['mail'.strval($i)];
-    }
-}
